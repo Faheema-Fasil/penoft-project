973367f43fa61a88e97a85f282b04706823b37cf
Faheema Fasil <faheemafasil12@gmail.com>
2025-06-06 14:51:16 +0530

    changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 function Footer() {
   return (
-
+    <>
+{/* 
     <div className='text-center  border-top  ' style={{
 
       width: "100%",
@@ -17,7 +18,8 @@ function Footer() {
       <p style={{ color: "#6f42c1", fontSize: "0.9rem" }} className='mt-2 mb-2'>
         Designed and Developed By: Penoft Technologies
       </p>
-    </div>
+    </div> */}
+    </>
   )
 }
 
